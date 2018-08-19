9bfd82f005e67093d0fd0645eec59d58bdfabef1
andytwoods <andytwoods@gmail.com>
2018-08-19 21:26:18 +0200

    get button duration distribution get click duration distribution

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//doing this in ES5 to encourage fresh developers to contribute (so they avoid the headache of ES6 / JS Fatigue)
+
 // let's run the function immediately, keeping it's contents entirely isolated from runtime
 (function () {
 
@@ -32,6 +34,83 @@
     api.get_button_durations = function () {
         JSON.parse(JSON.stringify(button_durations))
     };
+
+    //get the distribution of clicks from x to y ms in chunks of z milliseconds
+    api.get_click_dist = function(from, to, ms_chunks){
+       var filtered = filter_durations('click', from, to);
+       return down_time(filtered, ms_chunks);
+    };
+
+    api.get_button_dist = function(from, to, ms_chunks){
+        var filtered = filter_durations('button', from, to);
+        return down_time(filtered, ms_chunks)
+    };
+
+    function down_time(items, ms_chunks){
+        ms_chunks = ms_chunks || 1 * 1000; // 1 second chunks
+
+        var bins = [];
+
+        var item;
+        var bin;
+        var duration;
+        var down_time;
+
+        for(i=0;i<items.length;i++){
+            item = items[i];
+            duration = item['duration'];
+            down_time = item['down_time']
+            bin = Math.ceil(down_time / ms_chunks);
+            while(bins.length<bin+1){
+                bins.push([]);
+            }
+            bins[bin].push(duration);
+        }
+
+
+        var averages = [];
+        var list;
+        for(i=0;i<bins.length;i++){
+            list=bins[i];
+            averages.push(average(list))
+        }
+        return averages;
+    }
+
+    function average(list){
+        if(list.length===0) return '';
+        var total = 0;
+        for(var i=0;i<list.length;i++){
+            total += list[i];
+        }
+        return total / list.length;
+    }
+
+    function filter_durations(what, from, to){
+        from = from || 0;
+
+        var data;
+        if(what==='click') data = click_durations_x_positions;
+        else if(what==='button') data = button_durations;
+        else throw('unknown data type to use for distribution');
+
+        var item;
+        var down_time;
+        var filtered = []
+        for(var i=0;i<data.length;i++){
+            item = data[i];
+            down_time = item['down_time'];
+            if(to) {
+                if (down_time > to) break;
+            }
+            if(down_time > from){
+                filtered.push(item);
+            }
+        }
+
+        return filtered;
+    }
+
 
     // detect mouse movement and note mouse position with 'time from start'
     document.addEventListener("mousemove", mouseMoveListener);
@@ -94,9 +173,8 @@
         // need localised clone in case lots of quick button presses (and overwriting of initial var)
         var mouseData = JSON.parse(JSON.stringify(mouseDownData));
         mouseDownData = undefined;
-
         mouseData['up_time'] = up_time;
-        mouseData['pressed_dur'] = mouse_pressed_dur;
+        mouseData['duration'] = mouse_pressed_dur;
 
         setTimeout(function () {
             mouseData['postClick'] = gather_movements_between(up_time, up_time + api.postClickTime);
@@ -155,7 +233,7 @@
         keyDownData = undefined;
 
         buttonData['up_time'] = up_time;
-        buttonData['pressed_dur'] = key_pressed_dur;
+        buttonData['duration'] = key_pressed_dur;
 
         button_durations.push(buttonData);
     }
